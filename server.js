5856f3cbd1f2ad3e693d7d07ae884fb2dffb0eb1
Jude Nelson <judecn@gmail.com>
2018-07-31 15:29:12 -0400

    test blockstack 19.x

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 
-const port = 1700   // blockstack 17.xx
+const port = 1900   // blockstack 19.xx
 const app = express()
 app.use(cors())
 app.use('/', express.static(__dirname + '/public'))
